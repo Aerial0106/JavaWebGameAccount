a0bb92ce25637ef4fdd19f0d576f16415284f57c
Aerial0106 <quangdinh0106@gmail.com>
2024-06-10 20:29:22 +0700

    Tạo thêm controller

--- src/main/java/JavaWeb/GameAccount/controllers/HomeController.java
+++ src/main/java/JavaWeb/GameAccount/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package JavaWeb.GameAccount.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String hello(Model model) {
+        return "/layout";
+    }
 }
